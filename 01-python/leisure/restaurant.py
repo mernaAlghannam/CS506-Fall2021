92d14310eeab9e1272223f1f4eb9d860a13f2362
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2021-09-23 14:32:23 -0400

    drew a restaurant

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,3 +1,17 @@
 def draw_restaurant():
-    print("restaurant not found")
+    print("\u2585\u2585\u2585\u2585\u2585\u2585\u2585\u2585\u2585\u2585")
+    print("\u2588        \u2588")
+    print("┌─┬┐  ╔═╦╗  ╓─╥╖  ╒═╤╕")
+    print("│ ││  ║ ║║  ║ ║║  │ ││")
+    print("├─┼┤  ╠═╬╣  ╟─╫╢  ╞═╪╡")
+    print("└─┴┘  ╚═╩╝  ╙─╨╜  ╘═╧╛")
+    print("┌───────────────────┐")
+    print("│  ╔═══╗A restaurant│▒")
+    print("│  ╚═╦═╝            │▒")
+    print("╞═╤══╩══╤═══════════╡▒")
+    print("│ ├──┬──┤           │▒")
+    print("│ └──┴──┘           │▒")
+    print("└───────────────────┘▒")
+    print("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
+    print("\u1fbc9")
     return
